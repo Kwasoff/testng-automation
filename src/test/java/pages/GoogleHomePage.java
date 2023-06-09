ddc166eb96d60433df5baf9a87bbe7873cadbdaf
kwasoff <iharzsnko@gmail.com>
2023-06-08 21:50:12 -0500

    GoogleHomePage located searching box Use this location in Google class

--- src/test/java/pages/GoogleHomePage.java
+++ src/test/java/pages/GoogleHomePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -9,4 +11,6 @@ public class GoogleHomePage {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
+    @FindBy(name = "q")
+    public WebElement searchBox;
 }
--- src/test/java/tests/Google.java
+++ src/test/java/tests/Google.java
@@ -5,17 +5,20 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import pages.GoogleHomePage;
 import utilities.Config;
 import utilities.Driver;
 
 public class Google {
+
+    GoogleHomePage googleHomePage = new GoogleHomePage();
 
     @Test
     public void firstTest(){
         Driver.getDriver().get(Config.getValue("url"));
         Faker faker = new Faker();
         String word = faker.lorem().word();
-        Driver.getDriver().findElement(By.name("q")).sendKeys(word + Keys.ENTER);
+        googleHomePage.searchBox.sendKeys(word + Keys.ENTER);
         String title = Driver.getDriver().getTitle();
         Assert.assertTrue("Title failed", title.contains(word));
         Driver.quit();
