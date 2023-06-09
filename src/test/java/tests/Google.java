0a0fa47fc3ad57b70f639350cc94f1fee6d880b6
kwasoff <iharzsnko@gmail.com>
2023-06-08 20:46:08 -0500

    initial commit

---
+++ src/test/java/tests/Google.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.github.javafaker.Faker;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import utilities.Config;
+import utilities.Driver;
+
+public class Google {
+
+    @Test
+    public void firstTest(){
+        Driver.getDriver().get(Config.getValue("url"));
+        Faker faker = new Faker();
+        String word = faker.lorem().word();
+        Driver.getDriver().findElement(By.name("q")).sendKeys(word + Keys.ENTER);
+        String title = Driver.getDriver().getTitle();
+        Assert.assertTrue("Title failed", title.contains(word));
+
+
+    }
+
+}
